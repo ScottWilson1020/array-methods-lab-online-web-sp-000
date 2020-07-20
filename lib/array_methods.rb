8fd782601364845d3f431c48ff6e757643c5aaea
Scott Wilson <scottwilson1020@gmail.com>
2020-07-20 01:31:24 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,22 +1,26 @@
 def using_include(array, element)
-  array = [element];
-  return TRUE
-else 
-  return false
+  array.include?(element)
 end
 
 def using_sort(array)
-
+  array.sort!
 end
 
 def using_reverse(array)
-
+  array.reverse
 end
 
 def using_first(array)
-
+  array.first
 end
 
+def using_last(array)
+  array.last
+end
+
+def using_size(array)
+  array.size
+end
 def using_last(array)
 
 end
